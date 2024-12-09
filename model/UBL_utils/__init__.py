1aa83f3afb948b825bea52669ab41d528f567fb3
USTCzhr <zhr123456@mail.ustc.edu.cn>
2024-12-09 14:10:54 +0800

    model update

--- model/UBL_utils/__init__.py
+++ model/UBL_utils/__init__.py
@@ -1,13 +1,3 @@
 from .generate_utils import new_logits_processor, forbid_aa
-from .metrics import (compute_metrics_np, 
-                      compute_loss_structure_emb_similarity, 
-                      compute_metrics_generate_RNA_np, 
-                      compute_TE_regression_metrics, 
-                      compute_regression_metrics,
-                      compute_TEwithMFE_regression_metrics,
-                      compute_metrics_generate_decoder_RNA_np,
-                      compute_loss_full_protein_emb_similarity,
-                      compute_loss_structure_emb_similarity_MRL
-                      )   
 from .cache_utils import DynamicCache
 from .utils import *
