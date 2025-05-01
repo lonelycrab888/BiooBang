93ac766a7d0e0bb08a5f72ed3ef2fded7a876743
Heng-Rui Zhao <115449596+lonelycrab888@users.noreply.github.com>
2025-05-01 16:07:28 +0800

    Address the hallucination

--- generate_CDS.py
+++ generate_CDS.py
@@ -1,12 +1,14 @@
 from model.tokenization_UniBioseq import UBSLMTokenizer
 from model.modeling_UniBioseq import UniBioseqForCausalLM
-from model.UBL_utils import ForbidSequenceLogitsProcessor
+from model.UBL_utils import ForbidSequenceLogitsProcessor, CodonLogitsProcessor
 from transformers.generation.logits_process import LogitsProcessorList
 import torch
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import argparse
+
+from tqdm import tqdm
 
 def read_fa(fasta_path):
     fasta_dict = {}
@@ -33,30 +35,47 @@ def main():
     fasta_path = args.input_path
     save_path = args.save_path
     num_beams = args.num_beams 
+    print(fasta_path)
     excluding_sites = args.enzyme_cleavage_sites.split(',') if args.enzyme_cleavage_sites else None
+    print("excluding_sites: ", excluding_sites)
 
     print("load pretrained model!")
+    # Load the tokenizer and model
     tokenizer = UBSLMTokenizer.from_pretrained("pretrained-model/BiooBang-generationCDS")
     model = UniBioseqForCausalLM.from_pretrained("pretrained-model/BiooBang-generationCDS", device_map='auto')
 
-    logits_processor = LogitsProcessorList()
-
-    logits_processor.append(ForbidSequenceLogitsProcessor(tokenizer, excluding_sites))
-        
+    # Load the prompt data
     input_protein_dict = read_fa(fasta_path)
 
-    output_cds = {}
     print("start generation!")
-    for name, input_protein in input_protein_dict.items():
+    output_cds = {}
+    for name, input_protein in tqdm(input_protein_dict.items()):
+        if input_protein[0]=='M':
+            M_flag = True 
+        else:
+            M_flag =False
+            input_protein = 'M'+input_protein
         input_ids = torch.tensor([tokenizer.encode(input_protein)+[36]]).to(model.device)
         max_length = 4*len(input_protein)+6
+
+        # define the logits processor
+        logits_processor = LogitsProcessorList()
+        logits_processor.append(CodonLogitsProcessor(input_protein, tokenizer, len(input_protein)))
+        logits_processor.append(ForbidSequenceLogitsProcessor(tokenizer, excluding_sites))
+
+        # generate the sequence
         result = model.generate(input_ids, max_length = max_length, num_beams = num_beams, logits_processor=logits_processor, low_memory=True, num_return_sequences=1)
-        result_tok_list = []
+        
+        # decode the result
         result_tok = tokenizer.decode(result[0][len(input_protein)+3:].tolist()).replace(" ","").upper()
+        if M_flag == False:
+            result_tok = result_tok[3:]
         output_cds[name] = result_tok
+
     print(f"finish!")
     write_dict_to_fasta(output_cds, save_path)
     print(f"save results to {save_path}")
     
 if __name__ == '__main__':
     main()
+    
--- model/UBL_utils/generate_utils.py
+++ model/UBL_utils/generate_utils.py
@@ -22,6 +22,35 @@ sites = {
     'Dpn-I': ['gauc']
 }
 
+
+AMINO_ACID_TO_CODONS = {
+    'I': ['auu', 'auc', 'aua'],
+    'M': ['aug'],
+    'T': ['acu', 'acc', 'aca', 'acg'],
+    'N': ['aac', 'aau'],
+    'K': ['aaa', 'aag'],
+    'S': ['agc','agu','ucc', 'uca', 'ucg', 'uac'],
+    'R': ['agg','aga','cgu', 'cgc', 'cgg', 'cga'],
+
+    'V': ['guc', 'guu', 'gug', 'gca'],
+    'A': ['gca', 'gcc', 'gcu', 'gcg'],
+    'D': ['gac', 'gau'],
+    'E': ['gag', 'gaa'],
+    'G': ['ggc', 'ggu', 'ggg', 'gga'],
+
+    'F': ['uuc', 'uuu'],
+    'L': ['uua', 'uug', 'cua', 'cug', 'cuc', 'cuu'],
+    'Y': ['uac', 'uau'],
+    'C': ['ugu', 'ugc'],
+    'W': ['ugg'],
+
+    'P': ['ccu', 'ccc', 'cca', 'ccg'],
+    'H': ['cau', 'cac'],
+    'Q': ['caa', 'cag'],
+
+    '*': ['uaa', 'uag', 'uga'],
+}
+
 def forbid_aa():
     forbid_words = [i for i in range(31)]
     forbid_words.extend([i for i in range(35,41)])
@@ -64,3 +93,79 @@ class ForbidSequenceLogitsProcessor(LogitsProcessor):
                     if input_ids[batch_idx, -seq_len + 1:].tolist() == seq[:-1]:
                         scores[batch_idx, seq[-1]] = -float('inf')
             return scores
+
+
+
+
+class CodonLogitsProcessor(LogitsProcessor):
+    def __init__(self, amino_acid_seq, tokenizer, input_length):
+
+        self.amino_acids = amino_acid_seq
+        self.codon_table = AMINO_ACID_TO_CODONS
+        self.tokenizer = tokenizer
+        self.input_length = input_length
+        
+        self.id_to_token = {v: k for k, v in tokenizer.get_vocab().items()}
+        
+    def __call__(self, input_ids, scores):
+        current_step = input_ids.shape[-1]-3  #ignore special tokens
+        cds_position = current_step - self.input_length
+        
+        if cds_position < 0:
+            return scores
+        
+        codon_idx = cds_position // 3
+        pos_in_codon = cds_position % 3
+        
+        if codon_idx >= len(self.amino_acids)+3:
+            scores[:, :] = -float('inf')  
+            return scores
+        
+        if codon_idx < len(self.amino_acids):
+            current_aa = self.amino_acids[codon_idx]
+        else:
+            current_aa = '*'
+        
+        possible_codons = self.codon_table.get(current_aa, [])
+
+        for batch_idx in range(input_ids.shape[0]):
+            seq = input_ids[batch_idx]
+            
+            generated_cds = seq[self.input_length+3:]
+            
+            codon_start = codon_idx * 3
+            current_codon_chars = [
+                self.id_to_token.get(tok.item(), '') 
+                for tok in generated_cds[codon_start:codon_start+pos_in_codon]
+            ]
+            
+            allowed_chars = []
+            if pos_in_codon == 0:
+                allowed_chars = {codon[0] for codon in possible_codons}
+            elif pos_in_codon == 1:
+                valid_codons = [c for c in possible_codons 
+                               if len(c) > 0 and c[0] == current_codon_chars[0]]
+                allowed_chars = {c[1] for c in valid_codons}
+            elif pos_in_codon == 2:
+                valid_codons = [c for c in possible_codons 
+                               if len(c) >= 2 and 
+                               c[0] == current_codon_chars[0] and 
+                               c[1] == current_codon_chars[1]]
+                allowed_chars = {c[2] for c in valid_codons}
+            
+            allowed_ids = []
+            for char in allowed_chars:
+                token_id = self.tokenizer.convert_tokens_to_ids(char)
+                if token_id != self.tokenizer.unk_token_id:
+                    allowed_ids.append(token_id)
+            
+            if not allowed_ids:
+                scores[batch_idx, :] = -float('inf')
+                continue
+                
+            mask = torch.ones_like(scores[batch_idx]) * -float('inf')
+            for allowed_id in allowed_ids:
+                mask[allowed_id] = 0
+            scores[batch_idx] += mask
+            
+        return scores
